f988d602dedbfcb5b643980c1c6bed454167c826
Jakub Kwiatkowski <44145413+panjacob@users.noreply.github.com>
2022-04-02 12:05:17 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+# BrainBot
+
+### Jak postawić projekt:
+
+1) Wstawić plik 'files' do głównej ścieżki projektu z https://physionet.org/content/eegmmidb/1.0.0/
+2) python -m venv venv
+3) venv/Scripts/activate
+4) pip install -r requirements.txt
+
+### Aktualizacja requiremets:
+
+1) pip freeze > requirements.txt
---
+++ test.py
@@ -0,0 +1,17 @@
+import mne
+import os
+import matplotlib.pyplot as plt
+
+file = os.path.join('files', 'S001', 'S001R01.edf')
+data = mne.io.read_raw_edf(file)
+raw_data = data.get_data()
+info = data.info
+channels = data.ch_names
+
+x = raw_data[1, :]
+# S{numer_osoby}R{numer_zadania}_{numer_sygnalu_eeg(numer sondy?)-64_{nazwa_zadania}}
+title = "S001R01_1-64_Baseline, eyes open"
+plt.title(title)
+plt.plot(x)
+plt.show()
+plt.savefig(os.path.join('plots', "S001R01_1-64_Baseline, eyes open"))
