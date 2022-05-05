da10e55de7ea4010c7b949af8a38be2c3fc273ae
theATM <the_atm@wp.pl>
2022-05-05 19:20:37 +0200

    Braiset class

---
+++ brainset.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset
+
+
+class Brainset(Dataset):
+
+    def __init__(self):
+            pass
+
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+
+        return None
