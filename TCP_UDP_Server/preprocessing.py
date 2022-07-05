de1ce94cbfbd5bfd0ab5b5daa76afbb136adda73
froxec <picmego@gmail.com>
2022-07-05 11:11:40 +0200

    upd

--- TCP_UDP_Server/preprocessing.py
+++ TCP_UDP_Server/preprocessing.py
@@ -11,15 +11,4 @@ def filter_data(data, fs,  fc):
         data_filtered[:,i] = filtfilt(b, a, data[:, i])
     return data_filtered
 
-# fs = 1000
-# fc = 100
-# t = np.arange(1000)/ fs
-# y = np.sin(2*np.pi*t*100)
-# y = np.transpose(y)
-# y = y.reshape((1000, 1))
-# plt.plot(t, y, label='source')
-
-# y_filtered = filter_data(y, fs, fc)
-
-# plt.plot(t, y_filtered, label='filtered')
-# plt.show()
+# 
