e21b4a5a2cf57d28715d4307c3be7ac3f8d0f399
froxec <picmego@gmail.com>
2022-07-21 11:09:13 +0200

    six class experiment

--- Animation/experiment_tool/main.py
+++ Animation/experiment_tool/main.py
@@ -8,16 +8,37 @@ import numpy as np
 def generate_sequence(t, active_interval, rest_interval):
     return signal.square(2 * np.pi * SQUARE_FREQ * t, duty=DUTY)
 
-def show(side, canvas, left, right):
+def show(side, canvas, left_hand, right_hand, left_foot, right_foot, tongue):
     if (side == 0):
-        canvas.itemconfigure(left, state='normal')
-        canvas.itemconfigure(right, state='hidden')
+        canvas.itemconfigure(left_hand, state='normal')
+        canvas.itemconfigure(right_hand, state='hidden')
+        canvas.itemconfigure(left_foot, state='hidden')
+        canvas.itemconfigure(right_foot, state='hidden')
+        canvas.itemconfigure(tongue, state='hidden')
     elif (side == 1):
-        canvas.itemconfigure(right, state='normal')
-        canvas.itemconfigure(left, state='hidden')
+        canvas.itemconfigure(left_hand, state='hidden')
+        canvas.itemconfigure(right_hand, state='normal')
+        canvas.itemconfigure(left_foot, state='hidden')
+        canvas.itemconfigure(right_foot, state='hidden')
+        canvas.itemconfigure(tongue, state='hidden')
+    elif (side == 2):
+        canvas.itemconfigure(left_hand, state='hidden')
+        canvas.itemconfigure(right_hand, state='hidden')
+        canvas.itemconfigure(left_foot, state='normal')
+        canvas.itemconfigure(right_foot, state='hidden')
+        canvas.itemconfigure(tongue, state='hidden')
+    elif (side == 3):
+        canvas.itemconfigure(left_hand, state='hidden')
+        canvas.itemconfigure(right_hand, state='hidden')
+        canvas.itemconfigure(left_foot, state='hidden')
+        canvas.itemconfigure(right_foot, state='normal')
+        canvas.itemconfigure(tongue, state='hidden')
     elif (side == -1):
-        canvas.itemconfigure(left, state='hidden')
-        canvas.itemconfigure(right, state='hidden')
+        canvas.itemconfigure(left_hand, state='hidden')
+        canvas.itemconfigure(right_hand, state='hidden')
+        canvas.itemconfigure(left_foot, state='hidden')
+        canvas.itemconfigure(right_foot, state='hidden')
+        canvas.itemconfigure(tongue, state='hidden')
 
 def start(start_flag):
     start_flag[0] = True
@@ -30,8 +51,11 @@ if __name__ == '__main__':
     start_flag = [False]
     times = []
     target = []
-    left = canvas.create_rectangle(300, 300, 400, 400, fill='gray')
-    right = canvas.create_rectangle(800, 300, 900, 400, fill='gray')
+    left_hand = canvas.create_rectangle(300, 300, 400, 400, fill='gray')
+    right_hand = canvas.create_rectangle(800, 300, 900, 400, fill='gray')
+    left_foot = canvas.create_rectangle(300, 600, 400, 700, fill='blue')
+    right_foot = canvas.create_rectangle(800, 600, 900, 700, fill='blue')
+    tongue = canvas.create_rectangle(550, 100, 650, 200, fill='red')
     B = Button(window, text ="START", command =lambda: start(start_flag))
     B.pack()
     while(start_flag[0] == False):
@@ -45,16 +69,16 @@ if __name__ == '__main__':
         square = generate_sequence(time.time(), ACTION_TIME, REST_TIME)
         if (square == -1):
             rest_time = True
-            show(square, canvas, left, right)
+            show(square, canvas, left_hand, right_hand, left_foot, right_foot, tongue)
             side = -1
             print(side)
         elif(square == 1 and rest_time):
             rest_time = False
-            side = random.randint(0, 1)
-            show(side, canvas, left, right)
+            side = random.randint(0, 3)
+            show(side, canvas, left_hand, right_hand, left_foot, right_foot, tongue)
             print(side)
         elif(square == 1):
-            show(side, canvas, left, right)
+            show(side, canvas, left_hand, right_hand, left_foot, right_foot, tongue)
             print(side)
         times.append(time.time() - t_start)
         target.append(side)
